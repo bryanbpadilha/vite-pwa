9500a65a8a2f5a652e45b74b515a0c1964321f46
bryanbpadilha <bryanbpadilha@gmail.com>
2022-09-06 14:20:14 -0300

    minimal pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "safari-pinned-tab.svg",
+      ],
+      manifest: {
+        name: "Vite PWA",
+        short_name: "VitePWA",
+        description: "Test PWA app using Vite",
+        theme_color: "#282c34",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "safari-pinned-tab.svg",
+            sizes: "820x820",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
 });
