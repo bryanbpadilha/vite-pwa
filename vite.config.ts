c26a0b9853fb32041386449c249d1757f6976822
bryanbpadilha <bryanbpadilha@gmail.com>
2022-09-06 16:55:36 -0300

    testing globpatterns

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         enabled: true,
       },
       workbox: {
-        globPatterns: ["public/**.*"],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       includeAssets: [
         "favicon.ico",
